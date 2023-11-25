8b7f6fbf669e1998b197f76361c6094d8c9573a9
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-11-26 00:11:10 +0530

    localdb on local

--- devcompete/settings.py
+++ devcompete/settings.py
@@ -111,23 +111,30 @@ ASGI_APPLICATION = 'devcompete.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    # 'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv("DB_NAME"),
-    'USER': os.getenv("DB_USER"),
-    'PASSWORD': os.getenv("DB_PASS"),
-    'HOST': os.getenv("DB_HOST"),
-    'PORT': os.getenv("DB_PORT", 3306),
-    # 'SSL': 'ENABLED'
+if os.getenv('WEBSITE_HOSTNAME') is not None:
+    DATABASES = {
+        # 'default': {
+        #     'ENGINE': 'django.db.backends.sqlite3',
+        #     'NAME': BASE_DIR / 'db.sqlite3',
+        # }
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", 3306),
+        # 'SSL': 'ENABLED'
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
